6c1f326e574ca3c8b125e9125772261f911dfda9
Grewumi <zwowdy@gmail.com>
2012-12-28 19:34:01 +0800

    增加数据库和admin目录

---
+++ admin/admin_login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    	<meta http-equiv="Content-Type" content="text/html; charset=gbk" />
+		<meta http-equiv="Content-Language" content="zh-cn" />
+    	<script type="text/javascript" src="js/jquery-1.8.3.js"></script>
+		<title>管理页面</title>
+	</head>
+<body>
+
+</body>
+</html>
--- cpp_coder/linker.php
+++ cpp_coder/linker.php
@@ -3,4 +3,5 @@
     <a href="http://localhost/ideadiscover/cpp_coder/cpp_coder_1.php" title="cpp_coder">求职训练营第一天</a>
     <a href="http://localhost/ideadiscover/cpp_coder/cpp_coder_2.php" title="cpp_coder">求职训练营第二天</a>
     <a href="http://localhost/ideadiscover/cpp_coder/cpp_coder_3.php" title="cpp_coder">求职训练营第三天</a>
+    &nbsp;&nbsp;主讲:闵卫
 </nav>
